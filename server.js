108d8033f8a8de1afed51f59d5e328122ed4ac23
Christian Borella <info@christianborella.it>
2025-03-19 13:40:30 +0100

    Aggiunto il supporto per la variabile PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Public folder per esporre i file
 app.use('/uploads', express.static(path.join(__dirname, '/public/uploads')));
